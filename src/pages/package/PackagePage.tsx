4febc4afd74edab86212296eb54889996b2b3352
earl9rey <earl9rey@naver.com>
2026-05-25 18:42:06 +0900

    feat: 정책 모음 페이지 구현

--- src/pages/package/PackagePage.tsx
+++ src/pages/package/PackagePage.tsx
@@ -1,5 +1,23 @@
+import PackageCollectionSection from '@pages/package/components/PackageCollectionSection';
+import PolicyListSection from '@pages/package/components/PolicyListSection';
+import {
+  FILTER_GROUPS,
+  PACKAGE_ITEMS,
+  POLICY_ITEMS,
+  TOTAL_POLICY_COUNT,
+} from '@pages/package/constants/mockData';
+
 const PackagePage = () => {
-  return <div>Package Page</div>;
+  return (
+    <div className="min-h-full bg-background-muted">
+      <PackageCollectionSection items={PACKAGE_ITEMS} />
+      <PolicyListSection
+        filterGroups={FILTER_GROUPS}
+        policies={POLICY_ITEMS}
+        totalCount={TOTAL_POLICY_COUNT}
+      />
+    </div>
+  );
 };
 
 export default PackagePage;
